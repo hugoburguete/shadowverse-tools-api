edf10be52e6df729a2504d823624d0f0c2369edf
Hugo Burguete <hugoburguete@hotmail.com>
2024-03-31 20:51:52 +0100

    Remove unused DTOs

--- src/expansion/dto/update-expansion.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateExpansionInput } from './create-expansion.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateExpansionInput extends PartialType(CreateExpansionInput) {
-  @Field(() => Int)
-  id: number;
-}
