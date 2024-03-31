47e53d6cf7619bf1086f8066e79a3a075052f8e0
Hugo Burguete <hugoburguete@hotmail.com>
2024-03-31 19:52:52 +0100

    Add expansion service tests

--- src/expansion/expansion.service.spec.ts
+++ src/expansion/expansion.service.spec.ts
@@ -1,16 +1,66 @@
+import { getModelToken } from '@nestjs/sequelize';
+import { Test, TestingModule } from '@nestjs/testing';
+import expansionModelFactory from 'src/utils/test/expansion.model.factory';
+import { FindAllExpansionsArgs } from './dto/find-all-expansions.args';
+import { Expansion } from './entities/expansion.entity';
+import { ExpansionService } from './expansion.service';
+
 describe('ExpansionService', () => {
-  // let service: ExpansionService;
+  let service: ExpansionService;
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     providers: [ExpansionService],
-  //   }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ExpansionService,
+        {
+          provide: getModelToken(Expansion),
+          useValue: expansionModelFactory,
+        },
+      ],
+    }).compile();
 
-  //   service = module.get<ExpansionService>(ExpansionService);
-  // });
+    service = module.get<ExpansionService>(ExpansionService);
+  });
 
   it('should be defined', () => {
-    // expect(service).toBeDefined();
-    expect(true).toBe(true);
+    expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return all expansions', async () => {
+      const args = new FindAllExpansionsArgs();
+      args.attributes = {
+        fields: ['id'],
+        relations: {},
+      };
+      const result = await service.findAll(args);
+      expect(result.length).toBe(2);
+    });
+
+    it('should return a result with filtered expansions', async () => {
+      const args = new FindAllExpansionsArgs();
+      args.attributes = {
+        fields: ['id'],
+        relations: {},
+      };
+      args.ids = [1];
+      const result = await service.findAll(args);
+      expect(result.length).toBe(1);
+    });
+
+    describe('findOne', () => {
+      it('should return the queried expansion', async () => {
+        const expansionId = 1;
+        const result = await service.findOne(expansionId);
+
+        expect(result.id).toStrictEqual(expansionId);
+      });
+
+      it('should return the queried expansion', async () => {
+        const expansionId = 3;
+        const result = await service.findOne(expansionId);
+        expect(result).toBe(null);
+      });
+    });
   });
 });
